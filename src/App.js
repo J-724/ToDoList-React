a92445daf9130c60e7a7fbe6d541da3de1ef5a10
J-724 <jjpernian@gmail.com>
2022-08-18 00:00:46 -0400

    Working in localStorage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,56 @@
-
+import React, {useState, useEffect} from "react";
+import uniqid from "uniqid";
 import Main from "./components/Main/Main";
 import Sidebar from "./components/Sidebar/Sidebar";
+import { Project, Task } from "./components/#Misc/ObjTemplate";
+
+// change displayed project from click event that returns the name of the selected project on the sidebar
+
+function initStorage(object) {
+    if (localStorage.getItem(object) === null) {
+      return 'Storage empty';
+    } else {
+      return JSON.parse(localStorage[object].name);
+    }
+}
+
+
+const proj = {...Project}
+console.log(proj);
+localStorage.setItem('projects', [JSON.stringify(Project)]);
+
+const StorageObj = JSON.parse(localStorage.getItem('projects'));
+console.log(StorageObj);
+
 
 const App = () => {
+  const [selectedProject, setSelectedProject] = useState('');
+
+  const [projects, setProjects] = useState([
+    {
+      ...Project,
+      id: uniqid(),
+    },
+  ]);
+  
+  const [tasks, setTasks] = useState([
+    {
+      ...Task,
+      id: uniqid(),
+    },
+  ]);
+
+  const [test, setTest] = useState('Test message');
+
+  const [storageTest, setStorageTest] = useState(initStorage('projects'));
+
   return (
     <div className="app">
       <Sidebar />
-      <Main />
+      <Main 
+        test={test}
+        // storageTest={storageTest}
+      />
     </div>
   );
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,13 +1,18 @@
 import React, { useState, useEffect} from "react";
 import DisplayProjectsInfo from "./DisplayProjectsInfo";
 
-const Main = () => {
+const Main = (props) => {
 
   return (
     <div className="main">
+      {props.test}
+      <br/>
       <DisplayProjectsInfo />
     </div>
   );
 }
+
+
+// {props.storageTest}
 
 export default Main;
